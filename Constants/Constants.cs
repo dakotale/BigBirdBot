64942777fbeee1fca146ac82e813dddb35927a12
Dakota <dakotale@protonmail.com>
2025-11-04 21:12:50 -0500

    Final touches of revert and adding Constants.cs

---
+++ Constants/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace DiscordBot.Constants
+{
+    /// <summary>
+    /// Various static fields that are used throughout the bot.
+    /// This should be ignored by git so creds don't leak.
+    /// </summary>
+    public static class Constants
+    {
+        public const string discordBotConnStr = "Server=localhost;DataBase=DiscordBot;Integrated Security=true;TrustServerCertificate=True";
+        public const string botToken = "NTMyMzY3MDU4OTE1Mjk1MjMy.XJ_ypA.wgayR8pkXlju6Zwvv3YZus5n-xE";
+        public const string devBotToken = "MTE3NjI4NDg4NjYwOTQzMjU4Ng.GahGzJ.vASjBSTyU4FTKEgyu2Xfm5p9BwijXHtzq8-04Q";
+        public const string lavalinkUrl = "http://localhost:2333";
+        public const string lavaLinkPwd = "This 1s @ Sup3r S3cr3t P@ssword!";
+        public const string errorImageUrl = "https://cdn0.iconfinder.com/data/icons/shift-interfaces/32/Error-512.png";
+        public const string aiApiUserId = "1741944096";
+        public const string aiApiSecretId = "JLtYDfz2ghw3czdAn3yEee5T7a45zAUM";
+        public const string aiDetectorPath = @"C:\Temp\DiscordBot\AIDetector\";
+        public const string openAiToken = "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";
+        public const string openAiModel = "gpt-4.1";
+    }
+}
